ae2194ab874ebf0c3990c052be6386008dc85bd1
Bogdan Nicolae <bogdan.nicolae@acm.org>
2018-07-02 20:11:29 -0500

    logo fix

--- docs/conf.py
+++ docs/conf.py
@@ -75,6 +75,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+html_logo = '_templates/logo.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
