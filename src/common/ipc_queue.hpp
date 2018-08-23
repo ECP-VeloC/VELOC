bb7695831d3f45e0562ba51a3a24f5fa26aa856a
Bogdan Nicolae <bogdan.nicolae@acm.org>
2018-08-23 09:18:22 -0500

    code cleanup for strict C++11 compatibility

--- src/common/ipc_queue.hpp
+++ src/common/ipc_queue.hpp
@@ -107,9 +107,7 @@ template <class T> class shm_queue_t {
 	first_found->pending.pop_front();
 	first_found->progress.push_back(e);
 	DBG("dequeued element " << e);
-	return std::bind([this](container_t *q, const list_iterator_t &it, int status) { set_completion(q, it, status); },
-	                 first_found, std::prev(first_found->progress.end()), _1);
-	//return [this, q = first_found, it = std::prev(first_found->progress.end())](int status) { set_completion(q, it, status); };
+	return std::bind(&shm_queue_t<T>::set_completion, this, first_found, std::prev(first_found->progress.end()), _1);
     }
     size_t get_num_queues() {
 	return segment.get_num_named_objects();
