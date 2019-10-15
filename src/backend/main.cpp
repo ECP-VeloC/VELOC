a06510698d71f5f671d1eefa5affbee369bdcf97
Bogdan Nicolae <bogdan.nicolae@acm.org>
2019-10-15 18:23:34 -0500

    configurable parallelism on backend

--- src/backend/main.cpp
+++ src/backend/main.cpp
@@ -10,7 +10,7 @@
 #define __DEBUG
 #include "common/debug.hpp"
 
-const unsigned int MAX_PARALLELISM = 64;
+const unsigned int DEFAULT_PARALLELISM = 64;
 
 int main(int argc, char *argv[]) {
     if (argc < 2 || argc > 3) {
@@ -27,6 +27,11 @@ int main(int argc, char *argv[]) {
     if (argc == 3 && std::string(argv[2]) == "--disable-ec")
 	INFO("EC module not available in barebone version, disabled by default");
 
+    int max_parallelism;
+    if (!cfg.get_optional("max_parallelism", max_parallelism))
+        max_parallelism = DEFAULT_PARALLELISM;
+    INFO("Max number of client requests processed in parallel: " << max_parallelism);
+
     veloc_ipc::cleanup();
     veloc_ipc::shm_queue_t<command_t> command_queue(NULL);
     module_manager_t modules;
@@ -39,7 +44,7 @@ int main(int argc, char *argv[]) {
 	work_queue.push(std::async(std::launch::async, [=, &modules] {
 		    f(modules.notify_command(c));
 		}));
-	if (work_queue.size() > MAX_PARALLELISM) {
+	if (work_queue.size() > (unsigned int)max_parallelism) {
 	    work_queue.front().wait();
 	    work_queue.pop();
 	}
