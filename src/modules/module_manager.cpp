cf7525e2e338272547db5579ab7f8ccdea9f21d1
Bogdan Nicolae <bogdan.nicolae@acm.org>
2020-05-14 19:18:26 -0500

    fixed restart semantic for multi-level: stop early if success, keep trying if failure

--- src/modules/module_manager.cpp
+++ src/modules/module_manager.cpp
@@ -43,13 +43,17 @@ module_manager_t::~module_manager_t() {
 }
 
 int module_manager_t::notify_command(const command_t &c) {
-    int ret = VELOC_SUCCESS;
+    int ret = VELOC_FAILURE;
     for (auto &f : sig) {
 	int mod_ret = f(c);
-	if (c.command == command_t::TEST && mod_ret != VELOC_FAILURE)
-	    ret = std::max(ret, mod_ret);
-	else
-	    ret = std::min(ret, mod_ret);
+        if (c.command == command_t::RESTART) {
+            if (mod_ret == VELOC_SUCCESS)
+                return VELOC_SUCCESS;
+        } else {
+            if (mod_ret == VELOC_FAILURE)
+                return VELOC_FAILURE;
+        }
+        ret = std::max(ret, mod_ret);
     }
     return ret;
 }
