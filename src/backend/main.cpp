0850a13949a5d767e24833acfad4def3f30f4840
Bogdan Nicolae <bogdan.nicolae@acm.org>
2020-06-03 15:14:47 -0500

    default worker thread affinity to whole CPUSET

--- src/backend/main.cpp
+++ src/backend/main.cpp
@@ -6,11 +6,12 @@ using namespace ipc_queue;
 
 #include <queue>
 #include <future>
+#include <sched.h>
 
 #define __DEBUG
 #include "common/debug.hpp"
 
-const unsigned int MAX_PARALLELISM = 64;
+const unsigned int DEFAULT_PARALLELISM = 64;
 
 int main(int argc, char *argv[]) {
     bool ec_active = true;
@@ -37,6 +38,15 @@ int main(int argc, char *argv[]) {
 	DBG("Active backend rank = " << rank);
     }
 
+    int max_parallelism;
+    if (!cfg.get_optional("max_parallelism", max_parallelism))
+        max_parallelism = DEFAULT_PARALLELISM;
+    INFO("Max number of client requests processed in parallel: " << max_parallelism);
+    cpu_set_t cpu_mask;
+    long nproc = sysconf(_SC_NPROCESSORS_ONLN);
+    for (int i = 0; i < nproc; i++)
+        CPU_SET(i, &cpu_mask);
+
     backend_cleanup();
     backend_t<command_t> command_queue;
     module_manager_t modules;
@@ -47,9 +57,10 @@ int main(int argc, char *argv[]) {
     while (true) {
 	auto f = command_queue.dequeue_any(c);
 	work_queue.push(std::async(std::launch::async, [=, &modules] {
+                    sched_setaffinity(0, sizeof(cpu_set_t), &cpu_mask);
 		    f(modules.notify_command(c));
 		}));
-	if (work_queue.size() > MAX_PARALLELISM) {
+	if (work_queue.size() > (unsigned int)max_parallelism) {
 	    work_queue.front().wait();
 	    work_queue.pop();
 	}
