cc18a15f2c16dbad7ecb4baf05ae4f239c8f847a
Gregory Kosinovsky <kosinovsky1@llnl.gov>
2020-10-23 15:43:56 -0700

    debugging

--- auto-install.py
+++ auto-install.py
@@ -47,16 +47,13 @@ if __name__ == "__main__":
     if not os.path.isdir(args.prefix):
         print("Installation prefix {0} is not a valid directory!".format(args.prefix))
         sys.exit(1)
-    if os.path.isdir(args.temp):
-      try:
-        os.rmdir(args.temp)
-      except OSError as err:
+    if not os.path.isdir(args.temp):
         print("directory {0} wasn't empty".format(args.temp))
 #        print("Installation temporary directory {0} already exists, please remove and/or specify a different one!".format(args.temp))
 #        sys.exit(2)
-    try:
+      try:
          os.mkdir(args.temp)
-    except OSError as err:
+      except OSError as err:
         print("Cannot create temporary directory {0}!".format(args.temp))
         sys.exit(3)
 
