0696b65b22a5b57da68d04fd7d97bb1325a62351
Gregory Kosinovsky <kosinovsky1@llnl.gov>
2021-01-28 11:56:03 -0800

    debugging test/test_cleanup.sh

--- test/test_cleanup.sh
+++ test/test_cleanup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+echo -n "LISTING SCRATCH"
 ls -l $SCRATCH/*
+echo -n "LISTING PERSISTENT"
+ls -l $PERSISTENT/*
 
-rm -rf $SCRATCH/*
-rm -rf $PERSISTENT/*
+#rm -rf $SCRATCH/*
+#rm -rf $PERSISTENT/*
