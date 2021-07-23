1be889de0db906726ac258e02309a46b7ded8faf
Gregory Kosinovsky <kosinovsky1@llnl.gov>
2021-07-23 08:51:42 -0700

    added new tests

--- test/build_exec.sh
+++ test/build_exec.sh
@@ -6,3 +6,5 @@ LD_LIBRARY_PATH=$1/../deploy/lib64:$LD_LIBRARY_PATH
 mpicc -L$1/../deploy/lib64 -I$1/../deploy/include -I$1 -o $1/build/veloc_chkpt_mem_test $1/test/veloc_chkpt_mem_test.c -lveloc-client
 mpicc -L$1/../deploy/lib64 -I$1/../deploy/include -I$1 -o $1/build/veloc_chkpt_file_test $1/test/veloc_chkpt_file_test.c -lveloc-client
 
+mpicc -L$1/../deploy/lib64 -I$1/../deploy/include -I$1 -o $1/build/veloc_2chkpt_mem_test $1/test/veloc_2chkpt_mem_test.c -lveloc-client
+mpicc -L$1/../deploy/lib64 -I$1/../deploy/include -I$1 -o $1/build/veloc_2chkpt_file_test $1/test/veloc_2chkpt_file_test.c -lveloc-client
