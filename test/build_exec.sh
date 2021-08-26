7a26c594b5287af0a8118c02addaf5bf552da7fe
Gregory Kosinovsky <kosinovsky1@llnl.gov>
2021-08-26 09:13:33 -0700

    testing slurm in async threaded mode

--- test/build_exec.sh
+++ test/build_exec.sh
@@ -1,6 +1,8 @@
 module load cmake/3.18
-module load gcc/8.3
-module load boost
+#module load gcc/8.3
+#module load boost
+module load gcc/8.1
+module load boost/1.72.0
 OLD_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 LD_LIBRARY_PATH=$1/../deploy/lib64:$LD_LIBRARY_PATH
 mpicc -g -DNDEBUG -L$1/../deploy/lib64 -I$1/../deploy/include -I$1 -o $1/build/veloc_chkpt_mem_test $1/test/veloc_chkpt_mem_test.c -lveloc-client
