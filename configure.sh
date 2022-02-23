bc37ac786854fa9f6c3e8e93d35618f9778760a9
Bogdan Nicolae <bogdan.nicolae@acm.org>
2022-02-23 17:43:09 -0600

    configurable CMake options for testing

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export CMAKE_PREFIX_PATH=$HOME/deploy/share/cmake
-cmake -DWITH_AXL_PREFIX=$HOME/deploy -DWITH_ER_PREFIX=$HOME/deploy -DBOOST_ROOT=$HOME/deploy -DCMAKE_INSTALL_PREFIX=$HOME/deploy $1
+cmake -DCOMM_QUEUE=socket_queue -DWITH_AXL_PREFIX=$HOME/deploy -DWITH_ER_PREFIX=$HOME/deploy -DCMAKE_INSTALL_PREFIX=$HOME/deploy $1
