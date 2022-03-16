2d8d460be3977f314a3f7572f2bf8f8a139cae7c
Gregory Kosinovsky <kosinovsky1@llnl.gov>
2022-03-16 08:15:30 -0700

    removed pdsh dependency from multilevel corruption test and added performance test

---
+++ test/veloc_performance_mem_test.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <assert.h>
+
+#include "include/veloc.h"
+
+int main (int argc, char* argv[])
+{
+  int rank, ranks;
+  int chkpt_version;
+//  size_t filesize = 512*1024;
+  long filesize = 5120*1024*1024;
+
+//  MPI_Init(&argc, &argv);
+  int provided;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+  if (provided != MPI_THREAD_MULTIPLE) {
+      printf("MPI Init failed to provide MPI_THREAD_MULTIPLE");
+      exit (2);
+  }
+
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &ranks);
+
+  if (VELOC_Init(MPI_COMM_WORLD, argv[2]) != VELOC_SUCCESS) {
+
+      printf("Error initializing VELOC! Aborting...\n");
+
+      return 1;
+  }
+  /* make sure init completes*/
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  /* allocate space for the checkpoint data (make filesize a function of rank for some variation) */
+  //filesize = filesize + rank;
+  char* buf = (char*) malloc(filesize);
+  VELOC_Mem_protect(0, buf, filesize, sizeof(char));
+  /* time how long it takes to checkpoint */
+  MPI_Barrier(MPI_COMM_WORLD);
+  double ckpt_start = MPI_Wtime();
+  //init_buffer(buf, filesize, rank);
+  if(VELOC_Checkpoint("veloc_test", 1) != VELOC_SUCCESS){
+    printf("VELOC_Checkpoint FAILED\n");
+    return 1;
+  }
+  double ckpt_local = MPI_Wtime()-ckpt_start;
+  double ckpt_finish, ckpt_local_sum, ckpt_finish_max;
+  if(VELOC_Checkpoint_wait() == VELOC_SUCCESS){
+    ckpt_finish = MPI_Wtime()-ckpt_start;
+    MPI_Reduce(&ckpt_local, &ckpt_local_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&ckpt_finish, &ckpt_finish_max, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+  }
+  else{
+    printf("VELOC_Checkpoint FAILED\n");
+    return 1;
+  }
+  if (rank == 0) {
+    printf("computed Checkpoint times are: local Avg %8.6f s\tGlobal Max %8.6f  s\n", ckpt_local_sum/ranks, ckpt_finish_max);
+  }
+
+  if(buf != NULL) {
+    free(buf);
+    buf = NULL;
+  }
+  VELOC_Finalize(1);
+  MPI_Finalize();
+  return 0;
+}
+
