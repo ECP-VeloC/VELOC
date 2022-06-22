581b7856c633ee811bdd46db6a79e616fbc86a14
Gregory Kosinovsky <kosinovsky1@llnl.gov>
2022-06-22 11:16:17 -0700

    fixed lsf/slurm issues

---
+++ test/test_simulate_node_file_loss_slurm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#rm -rf $SCRATCH/*
+rm -rf /tmp/scratch/*
+
+
---
+++ test/test_simulate_scratch_and_persistent_loss_slurm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#rm -rf $SCRATCH/*
+#rm -rf $PERSISTENT/*
+rm -rf /g/g19/kosinov/veloc_dir/persistent/*
+rm -rf /tmp/scratch/*
+
+
